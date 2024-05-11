5b02271d6f5ae2e87ab20431b4f208638cbc1667
Nathan Humphrey <NathanH@nait.ca>
2024-05-10 20:02:02 -0600

    Add lotto ticket exercise

--- week_01_dart_demo/bin/week_01_dart_demo.dart
+++ week_01_dart_demo/bin/week_01_dart_demo.dart
@@ -1,8 +1,15 @@
 import 'dart:io';
 
+import 'package:week_01_dart_demo/person.dart';
 import 'package:week_01_dart_demo/week_01_dart_demo.dart';
 
 void main(List<String> arguments) {
   String name = prompt('Enter your name: ');
-  stdout.writeln('Hello $name, welcome to DMIT2504!');
+  int age = promptInt('Enter your age: ');
+
+  // TODO: deal with the potential for a negative age
+  Person person = Person(name, age);
+
+  stdout.writeln(
+      'Hello ${person.name}, it looks like you are ${person.age} years old.');
 }
--- week_01_dart_demo/lib/week_01_dart_demo.dart
+++ week_01_dart_demo/lib/week_01_dart_demo.dart
@@ -1,5 +1,21 @@
 import 'dart:io';
 
+/// Returns an [int]
+///
+/// Will continue to prompt the user until a valid [int] is entered.
+/// Displayst the [message] as the prompt for the user.
+int promptInt(String message) {
+  bool isValid = false;
+
+  while (!isValid) {
+    try {
+      return int.parse(prompt(message));
+    } catch (e) {
+      stdout.writeln('Invalid input. Please enter a whole number.');
+    }
+  }
+}
+
 /// Returns a [String]
 ///
 /// Displays the passed in [message] as the prompt
