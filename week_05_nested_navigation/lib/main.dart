a653bf57db37113ac8a4cfcc5736059dd0aeb588
Nathan Humphrey <NathanH@nait.ca>
2024-06-07 20:24:18 -0600

    Update nested navigation

--- week_05_nested_navigation/lib/main.dart
+++ week_05_nested_navigation/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:week_05_nested_navigation/pages/home_page.dart';
 import 'package:week_05_nested_navigation/pages/settings/settings_manager.dart';
-import 'package:week_05_nested_navigation/pages/settings/settings_page.dart';
 import 'package:week_05_nested_navigation/routes.dart';
 
 void main() {
