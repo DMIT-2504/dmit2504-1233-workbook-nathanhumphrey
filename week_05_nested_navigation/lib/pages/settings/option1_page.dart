023b9cf4bd4978101be924db4c3a8e252c620a48
Nathan Humphrey <NathanH@nait.ca>
2024-06-10 19:37:19 -0600

    Update option 1 UI

--- week_05_nested_navigation/lib/pages/settings/option1_page.dart
+++ week_05_nested_navigation/lib/pages/settings/option1_page.dart
@@ -1,13 +1,44 @@
 import 'package:flutter/material.dart';
 
-class Option1Page extends StatelessWidget {
+class Option1Page extends StatefulWidget {
   const Option1Page({super.key});
 
   @override
+  State<Option1Page> createState() => _Option1PageState();
+}
+
+class _Option1PageState extends State<Option1Page> {
+  final _textController = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text('Option 1 page'),
+        child: Column(
+          children: [
+            const Text('Option 1 - Shared Preferences'),
+            Padding(
+              padding: const EdgeInsets.all(
+                16.0,
+              ),
+              child: TextField(
+                controller: _textController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Some Text',
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Save Text'),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Remove Text'),
+            ),
+          ],
+        ),
       ),
     );
   }
