86884cb617f96da52c774a7c1ed6eaab293b4bdb
Nathan Humphrey <NathanH@nait.ca>
2024-07-03 20:51:58 -0600

    Update week 09 - AnimatedBuilder

--- week_09_animations/lib/animations_page.dart
+++ week_09_animations/lib/animations_page.dart
@@ -1,3 +1,4 @@
+import 'dart:math' as math;
 import 'package:flutter/material.dart';
 
 class AnimationsPage extends StatefulWidget {
@@ -7,9 +8,45 @@ class AnimationsPage extends StatefulWidget {
   State<AnimationsPage> createState() => _AnimationsPageState();
 }
 
-class _AnimationsPageState extends State<AnimationsPage> {
+class _AnimationsPageState extends State<AnimationsPage>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(
+        seconds: 3,
+      ),
+    )..repeat();
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('Foobar');
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Animations Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AnimatedBuilder(
+              animation: _controller,
+              child: const Text('Rotate Animation'),
+              builder: (BuildContext context, Widget? child) {
+                return Transform.rotate(
+                  angle: _controller.value * (2 * math.pi),
+                  child: child,
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
