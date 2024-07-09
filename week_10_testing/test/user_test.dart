cc087d61b40281a129c4698a3623360b6f8759dd
Nathan Humphrey <NathanH@nait.ca>
2024-07-08 19:33:37 -0600

    Add week 10 testing

---
+++ week_10_testing/test/user_test.dart
@@ -0,0 +1,20 @@
+import 'package:test/test.dart';
+import 'package:week_10_testing/models/user.dart';
+
+void main() {
+  group('User Tests', () {
+    late User user;
+
+    setUp(() {
+      user = const User(
+        name: 'Jane Doe',
+        email: 'jdoe@example.com',
+      );
+    });
+
+    test('User should have a name and an email', () {
+      expect(user.name, 'Jane Doe');
+      expect(user.email, 'jdoe@example.com');
+    });
+  });
+}
